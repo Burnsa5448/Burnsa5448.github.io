0e0a79db9695d9f4e4b83de3922623622640f3ee
Burnsa5448 <burnsa5448@student.faytechcc.edu>
2024-10-01 13:30:34 -0400

    Add files via upload

---
+++ python_test_burnsA.py
@@ -0,0 +1,38 @@
+# CTI 110
+# p1LAB1 - My First Page
+# burnsa
+# 09/24/24
+
+#just dpoing phantom of the opera things to try my tests
+print("Hello world")
+#See if they like music
+print("Trying out Python for the first time.")
+print("If I am the phantom, it is because man's hatred has made me so. - OG")
+#you can use single quotes or double quotes
+print('\tA better way to quote would be\t\"If I am the Phantom. it is because man and hatered has made me so." -OG')
+
+print("Newline\n is kind of like \n<br>\n")
+print("\\ is the backslash")
+print("\tTabs space out\t\words")
+
+
+print("Hello. Do you like music?")
+#greet the user by name
+#TODO: ask their name
+print("What is your name? ")
+print("Enter first name:")
+first=input()
+print("Enter last name:")
+last=input()
+
+#greet the user
+print("Nice to meet you,", first, last)
+
+#ask them if they like music again
+print ("Do you like music?")
+topic=input()
+print("Oh good, you said", topic, "too.")
+
+#first = input("then insert the question")
+
+
