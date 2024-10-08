07f704ce0876d2ada89c85637aa44c73e951f4dd
Burnsa5448 <burnsa5448@student.faytechcc.edu>
2024-10-08 17:45:40 +0000

    started the code. got up to destination question

---
+++ P1HW2_burnsa.py
@@ -0,0 +1,21 @@
+# CTI 110
+# P1HW2
+# A Burns
+# 10/08/24
+
+# This is a travel budget program
+""""
+3. Ask user to enter budget
+4. Ask destination
+5. Amount they spend on gas or tickets
+6. Amount on accomadtion 
+7. Amount on food
+8. Add Expenses
+9. Subtract expenses from budget
+10. Display results
+"""
+print("🛫 Trip Calculator 🛬")
+# Ask  user their budget 
+budget=float(input("What is your travel budget? $ "))
+# Where is it
+destination=input("Where are you going? ")
